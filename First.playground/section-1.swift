21deec01ada2c83c9b9d1927e6a63a2ebbb66c9a
Victor <em3888@gmail.com>
2016-11-22 22:19:21 +0800

    add change to lession 1

--- First.playground/section-1.swift
+++ First.playground/section-1.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
-label.backgroundColor = UIColor.lightGrayColor()
+label.backgroundColor = UIColor.lightGrayColor
 var x = 0
 for i in 1...100 {
     x = i * i
